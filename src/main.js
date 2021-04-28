256237b6747de1f1578740168ee92c6f170addb8
Павел Виноградов <p.vinogradov@itwebnet.ru>
2021-04-28 13:36:45 +0300

    init

--- src/main.js
+++ src/main.js
@@ -6,11 +6,9 @@ import store from "./store";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import VueClickAway from "vue3-click-away";
-import { VueReCaptcha } from "vue-recaptcha-v3";
 
 import "@/assets/scss/app.scss";
 createApp(App)
-  .use(VueReCaptcha, { siteKey: "6LcFtJcaAAAAAL7kmqXxgX7ZkObNp-C-ceNTK8V-" })
   .use(VueClickAway)
   .use(VueAxios, axios)
   .use(store)
