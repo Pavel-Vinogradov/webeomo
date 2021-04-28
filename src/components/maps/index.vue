b7b826f41a5cc0a516a086f7d32e021b366e71b9
Павел Виноградов <p.vinogradov@itwebnet.ru>
2021-04-28 13:43:45 +0300

    init

--- src/components/maps/index.vue
+++ src/components/maps/index.vue
@@ -1,22 +1,12 @@
 <template>
-  <GoogleMap
-    api-key="AIzaSyA5N7UHT6_K-STVcwug9tBd0eku_JzV7sg"
-    style="width: 700px; height: 600px"
-    :center="center"
-    :zoom="17"
-  >
-    <Marker :options="{ position: center }" />
-  </GoogleMap>
+
+  <div></div>
 </template>
 
 <script>
-import { GoogleMap, Marker } from "vue3-google-map";
+
 export default {
   name: "indexMaps",
-  components: { GoogleMap, Marker },
-  setup() {
-    const center = { lat: 58.59515481848332, lng: 49.68966170209393 };
-    return { center };
-  }
+
 };
 </script>
